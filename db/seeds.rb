7d2c6ec7b02452e3b9969010d19de8b92dde5456
Nico Proto <nicolasproto@gmail.com>
2021-04-18 12:28:47 +0200

    ARCH: Seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+def line
+  puts "---------------------------"
+end
+
+puts "Cleaning up database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+puts "Database clean!"
+
+line
+
+url = "http://tmdb.lewagon.com/movie/top_rated?language=en-US&page=1"
+
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "Creating movies..."
+movies["results"].each do |movie|
+  if movie["original_language"] == "en"
+    movie = Movie.create!(
+      title: movie["original_title"],
+      overview: movie["overview"],
+      poster_url: "https://image.tmdb.org/t/p/w500" + movie["poster_path"],
+      rating: movie["vote_average"]
+    )
+    puts movie.title + " created!"
+    line
+  end
+end
+
+puts "Seeding done!"
