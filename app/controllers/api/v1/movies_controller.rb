f6210192b303ba1803e411a64a47102f25d32cb2
Nico Proto <nicolasproto@gmail.com>
2021-04-18 12:35:01 +0200

    ARCH: Setup index endpoint for movies

---
+++ app/controllers/api/v1/movies_controller.rb
@@ -0,0 +1,13 @@
+class Class Api::V1::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+    limit = params[:_limit]
+
+    if limit.present?
+      limit = limit.to_i
+      @movies = @movies.last(limit)
+    end
+
+    render json: @movies.reverse
+  end
+end
