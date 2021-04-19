2eafd6ce98dc24a44d787d670dae1e3b122d5cfb
Nico Proto <nicolasproto@gmail.com>
2021-04-19 16:23:53 +0200

    FEAT: Added content for Not Found page

--- watch-list-vue-frontend/src/views/NotFound.vue
+++ watch-list-vue-frontend/src/views/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <section>NOT FOUND</section>
-  </div>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found - maybe check out all our <router-link to="/movies">movies</router-link></p>
+    </base-card>
+  </section>
 </template>
