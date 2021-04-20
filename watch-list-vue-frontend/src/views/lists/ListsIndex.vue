bd08917715bd27a521e7a653b83a0110814cb509
Nico Proto <nicolasproto@gmail.com>
2021-04-20 19:29:52 +0200

    FIX: Removed duplicated index items

--- watch-list-vue-frontend/src/views/lists/ListsIndex.vue
+++ watch-list-vue-frontend/src/views/lists/ListsIndex.vue
@@ -3,16 +3,16 @@
     <section>FILTERS</section>
     <section>
       <base-card>
-        <ul v-if="hasLists">
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul v-else-if="hasLists">
           <list-item
             v-for="list in lists"
             :key="list.id"
             :id="list.id"
             :name="list.name"
           ></list-item>
-          <li v-for="list in lists" :key="list.id">
-            {{ list.name }}
-          </li>
         </ul>
         <h3 v-else>No list found.</h3>
       </base-card>
@@ -28,6 +28,11 @@ export default {
   components: {
     ListItem,
   },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
   computed: {
     ...mapGetters({
       lists: "lists/lists",
@@ -39,11 +44,13 @@ export default {
       loadLists: "lists/loadLists",
     }),
     async setLists() {
+      this.isLoading = true;
       try {
         await this.loadLists();
       } catch (error) {
         // TODO: Handle error
       }
+      this.isLoading = false;
     },
   },
   created() {
@@ -51,3 +58,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
