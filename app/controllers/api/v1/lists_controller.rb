ffb3b7019fbbc17ed9ac5f1e0abd694c0ef428d0
Nico Proto <nicolasproto@gmail.com>
2021-04-20 15:58:35 +0200

    FEAT: Built list controller for api endpoints

---
+++ app/controllers/api/v1/lists_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::ListsController < ApplicationController
+  before_action :set_list, only: [ :show, :update, :destroy ]
+
+  def index
+    @lists = List.all
+  end
+
+  def show; end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def update
+    if @list.update(list_params)
+      render :show, status: :accepted
+    else
+      render json: { json: @list.errors, status: :unprocessable_entity }
+    end
+  end
+
+  def destroy
+    if @list.destroy
+      render json: { status: :ok, message: 'List destroyed' }
+    else
+      render json: { json: @list.errors, status: :unprocessable_entity }
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :overview, :poster_url, :rating)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
