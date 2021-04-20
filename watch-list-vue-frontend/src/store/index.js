0b1c1a1e6271af4d0fe949fd31826bb32948cfd6
Nico Proto <nicolasproto@gmail.com>
2021-04-20 16:35:01 +0200

    FEAT: Setup Vuex store for lists

--- watch-list-vue-frontend/src/store/index.js
+++ watch-list-vue-frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 
 import moviesModule from "./modules/movies/index.js";
+import listsModule from "./modules/lists/index.js";
 
 export default createStore({
   modules: {
     movies: moviesModule,
+    lists: listsModule,
   },
 });
